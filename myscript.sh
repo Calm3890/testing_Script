48effd78c090dcaff35330d956ce7ffbf682dede
Calm3890 <vielfade@yahoo.com>
2020-03-17 13:05:24 +0700

    testing push

---
+++ myscript.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# ECHO COMMAND
+#echo Hello World!
+
+#VARIABLES
+#Uppercase by convention
+#Letters,number,underscore
+NAME="JACK"
+#echo "MY NAME IS $NAME"
+#echo "MY NAME IS ${NAME}"
+mkdir exercise
+
+cp . exercise
+
+
+#User Input
+#read -p "input something = " NAME
+#echo "MY NAME IS ${NAME}"
+
+#Simple if statement
+# if [ "$NAME" == "BRAD" ]
+# then
+#     "HELLO $NAME"
+# fi
+
+#IF ELSE statement
+# if [ "$NAME" == "BRAD" ]
+# then
+#    echo "your name is BRAD"
+# else
+#    echo "your name isn't brad"
+# fi
+
+#ELIF
+# if [ "$NAME" == "BRAD" ]
+# then
+#    echo "your name is BRAD"
+# elif [ "$NAME" == "JACK" ]
+# then
+#    echo "your name is JACK"
+# else
+#    echo "your name isn't brad or jack"
+# fi
